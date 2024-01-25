e12d5328db6dd50744d749909473be96b580ef88
Ajay Kumar <104864110+ajaythink@users.noreply.github.com>
2024-01-25 15:03:11 +0000

    Heap memory,

---
+++ Pattern/String/BasicString.java
@@ -0,0 +1,12 @@
+public class BasicString{
+    public static void main(String[] args){
+        //String Letral format
+        String s = "literal";
+
+        String s1 = new String("Literal");
+        String s2 = "Ajay";
+        String s3 = s2.concat("Kumar");
+        System.out.println(s3);
+
+    }
+}
